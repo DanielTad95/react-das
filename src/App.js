84e865b51a3c70e94e438d370a7d6e9af568abbc
DanielTad95 <daniel17tad@gmail.com>
2019-07-04 09:38:11 -0700

    Bajanel maseri

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
 import React from 'react';
+import Header from './components/Header';
+import Content from './components/Content';
+import SideBar from './components/SideBar';
+import Footer from './components/Footer';
+
 import './App.css';
+class App extends React.Component {
+    state = {
+        country: "Armenia"
+    }
+    constructor(props) {
+        super(props);
+        this.handleOnChengeCoutry = this.handleOnChengeCoutry.bind(this);
+    }
 
-class App extends React.Component{
-  state = {
-    coutry:"armenia"
-  }
-  constructor(props){
-    super(props);
-    console.log("constructor")
-  }
+    handleOnChengeCoutry() {
+        this.setState({ country: "USA" });
+    }
 
-  componentDidMount(){
-    console.log("componentDidMount")
-  }
-
-  static getDerivedStateFromProps(){
-    console.log("getDerivedStateFromProps");
-    return null;
-  }
-  render() {
-    console.log("render")
-    return (
-      <div>
-        hello world
-      </div>  
-    )
-  }
+    render() {
+        return (
+            <>
+                <Header />
+                <SideBar />
+                <Content />
+                <Footer />
+            </>
+        )
+    }
 }
-
 export default App;
-
- 
